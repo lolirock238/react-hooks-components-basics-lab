f5a5d65c570592bfc8d6390159223288c25efd12
Nicole kibe <nicolekibe822@gmail.com>
2025-10-20 10:58:45 +0300

    add all changes

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// ✅ Write the <About> component here
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {/* ✅ Add the <Home> component here */}
+      <Home />
+      {/* ✅ Add your <About> component here */}
+      <About />
     </div>
   );
 }
